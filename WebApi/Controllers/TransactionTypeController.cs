08a026d265f23c9669c02e510cdcd3f3b8f92c69
ltolentino <luistolentino27@gmail.com>
2022-03-26 17:39:38 -0400

    add: transaction type controller

---
+++ WebApi/Controllers/TransactionTypeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Services.TransactionTypeService;
+using Application.Services.TransactionTypeService.Dto;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionTypeController : ControllerBase
+    {
+        private readonly ITransactionTypeService _service;
+
+        public TransactionTypeController(ITransactionTypeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TransactionTypeDto>>> Get() =>
+            Ok(await _service.GetTransactionTypeListAsync());
+    }
+}
