09732d20db390fe5b7ffc65f8bd4e16aa20f8d82
ltolentino <luistolentino27@gmail.com>
2022-03-26 13:34:30 -0400

    add: unit controller

---
+++ WebApi/Controllers/UnitController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Services.UnitService;
+using Application.Services.UnitService.Dto;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitController : ControllerBase
+    {
+        private readonly IUnitService _service;
+
+        public UnitController(IUnitService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UnitDto>>> Get()
+        {
+            return Ok(await _service.GetUnitListAsync());
+        }
+    }
+}
